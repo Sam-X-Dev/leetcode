aa83112cb7b631c56753b8a881375bca4a048642
Samarth Swami <samarthswami66@gmail.com>
2026-04-27 20:23:15 +0530

    Time: 12 ms (16.62%), Space: 29.3 MB (44.08%) - LeetHub

---
+++ 3684-maximize-sum-of-at-most-k-distinct-elements/3684-maximize-sum-of-at-most-k-distinct-elements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> maxKDistinct(vector<int>& nums, int k) {
+    
+        set<int, greater<int>> st(nums.begin(), nums.end());
+
+        vector<int> res;
+
+        for (int x : st) {
+ 
+            if (res.size() == k) break;
+
+            res.push_back(x);
+        }
+
+        return res;
+    }
+};
