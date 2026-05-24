b4472d8d6ac329a92a4a5befbbe3b6931ca1af21
Samarth Swami <samarthswami66@gmail.com>
2026-05-24 18:07:01 +0530

    Time: 0 ms (100%), Space: 10.9 MB (17.11%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -1,26 +1,22 @@
-#include <algorithm>
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
+
+        unordered_map<int,int> um;
+
         int sum = 0;
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-
-        if(n == 1) return nums[0];
-        if(nums[0] != nums[1]){sum+=nums[0];}
-        if(nums[n-1] != nums[n-2]){sum+=nums[n-1];}
-
-        for(int i = 1; i<n-1 ; i++){
-
-            bool condition = ((nums[i] != nums[i-1]) && (nums[i] != nums[i+1]));
-            if(condition){
-
-                sum+=nums[i];
-
-            }
-
-            
+        
+        for(int i = 0; i < n;i++){
+            um[nums[i]]++;
         }
+            
+        for(auto x:um){
+            if(x.second == 1){
+               sum+= x.first;
+            }
+        }    
         return sum;
     }
+       
 };
