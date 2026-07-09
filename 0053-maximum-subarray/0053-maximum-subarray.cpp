bbc66bf15f717a968cf5189c2e803c73f467638b
Samarth Swami <samarthswami66@gmail.com>
2026-07-09 15:19:24 +0530

    Time: 0 ms (100%), Space: 71.8 MB (14.51%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,22 +1,14 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int prefixSum = 0;
-        int minPrefix = 0;
-        int answer = nums[0];
+       int tp= INT_MIN, n = nums.size();
+        int cntsum = 0;
+        for(int i = 0; i<n; i++){
+             cntsum+=nums[i];
+            tp = max(cntsum,tp);
 
-        for (int i = 0; i < nums.size(); i++) {
-            prefixSum += nums[i];
-
-            answer = max(answer, prefixSum - minPrefix);
-
-            minPrefix = min(minPrefix, prefixSum);
+            if(cntsum<0) cntsum=0;
         }
-
-        return answer;
+        return tp;
     }
 };
