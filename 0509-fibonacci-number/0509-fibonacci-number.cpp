0868a001dcc6c4a591cb2050fbeae5aa1c82650a
Samarth Swami <samarthswami66@gmail.com>
2026-07-12 15:13:59 +0530

    Time: 14 ms (22.98%), Space: 7.7 MB (65.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,25 +1,8 @@
 class Solution {
 public:
         int fib(int n) {
-        // Base case:
-        // If n is 0 or 1, return n directly
-        if (n <= 1) return n;
-
-        // prev2 = F(0), prev1 = F(1)
-        int prev2 = 0;
-        int prev1 = 1;
-
-        // Loop from 2 to n to build Fibonacci iteratively
-        for (int i = 2; i <= n; i++) {
-            // Current Fibonacci number
-            int curr = prev1 + prev2;
-
-            // Shift values for next iteration
-            prev2 = prev1;
-            prev1 = curr;
-        }
-
-        // Final result stored in prev1
-        return prev1;
+            if(n==0) return 0;
+            if(n==1) return 1;
+            return fib(n-1) + fib(n-2);
     }
 };
