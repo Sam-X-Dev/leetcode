44df405b1a7e5448eca19d657614a47103240c8b
Samarth Swami <samarthswami66@gmail.com>
2026-08-05 19:33:42 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        int n = s.size();
+
+        if(n==1 || n%2 !=0) return false;
+
+        for(int i = 0; i < n; i++){
+            if(s[i]=='(' || s[i] == '[' || s[i] == '{'){
+                st.push(s[i]);
+            } else { 
+                if(st.size() == 0) return false;
+                if(s[i] == ')' && st.top() == '('){
+                st.pop();
+                } else if(s[i] == ']' && st.top() == '['){
+                    st.pop();
+                } else if(s[i] == '}' && st.top() == '{'){
+                    st.pop();
+                } else return false;
+            }
+        }
+
+        return st.size() == 0;
+    }
+};
